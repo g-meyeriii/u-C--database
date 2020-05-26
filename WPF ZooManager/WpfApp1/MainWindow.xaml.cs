689abe686468fc67bbe8964664657dda93f455fc
gjmiii9@gmail.com <gjmiii9@gmail.com>
2020-05-26 15:07:44 -0700

    WPF database init commit

---
+++ WPF ZooManager/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WpfApp1
+{
+  
+    public partial class MainWindow : Window
+    {
+        SqlConnection sqlConnection;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["WpfApp1.Properties.Settings.UdemyClassDBConnectionString"].ConnectionString;
+            sqlConnection = new SqlConnection(connectionString);
+            ShowZoos();
+        }
+        private void ShowZoos()
+        {
+            try 
+            {
+                string query = "select * from Zoo";
+                // the SQLDataAdapter, like an interface to make tables usable by C#-Objects
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+
+                using (sqlDataAdapter)
+                {
+                    DataTable zooTable = new DataTable();
+
+                    sqlDataAdapter.Fill(zooTable);
+
+                    //Which Information of the Tavle in DataTable should be shone on our ListBox? 
+                    listZoos.DisplayMemberPath = "Location";
+                    //Which Value should be delivered, when an Item from our ListBox is selected? 
+                    listZoos.SelectedValuePath = "Id";
+                    //The Reference to the Data the ListBox should populate
+                    listZoos.ItemsSource = zooTable.DefaultView;
+
+                }
+
+            } 
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        private void ShowAssociatedAnimals()
+        {
+            try
+            {
+                string query = "select * from Animal a inner join ZooAnmimal " +
+                    "za on a.Id = za.AnimalId where za.ZooId= @ZooId " ;
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                
+                // the SQLDataAdapter, like an interface to make tables usable by C#-Objects
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+
+                using (sqlDataAdapter)
+                {
+
+                    sqlCommand.Parameters.AddWithValue("@ZooId", listZoos.SelectedValue);
+                    
+                    DataTable animalTable = new DataTable();
+
+                    sqlDataAdapter.Fill(animalTable);
+
+                    //Which Information of the Tavle in DataTable should be shone on our ListBox? 
+                    listAssociatedAnimals.DisplayMemberPath = "Name";
+                    //Which Value should be delivered, when an Item from our ListBox is selected? 
+                    listAssociatedAnimals.SelectedValuePath = "Id";
+                    //The Reference to the Data the ListBox should populate
+                    listAssociatedAnimals.ItemsSource = animalTable.DefaultView;
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        private void listZoos_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ShowAssociatedAnimals();
+        }
+    }   
+}
